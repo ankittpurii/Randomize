a7d5e5739cdb37c57bbb156a5f81d98c6cde87af
ankittpurii <puriankit31@gmail.com>
2024-07-11 23:27:00 +0530

    Created a basic UI for Randomize with constraint layout

--- app/src/main/java/com/example/randomize/MainActivity.kt
+++ app/src/main/java/com/example/randomize/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.randomize
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton=findViewById<Button >(R.id.rollButton)
+        val resultsTextView=findViewById<TextView >(R.id.resultsTextView)
+        val seekBar=findViewById<SeekBar>(R.id.seekBar)
     }
 }
